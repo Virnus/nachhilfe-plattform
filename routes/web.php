c1a05433a1c1bfd28339617a1e733bf6cafb053d
Virnus <seralichtenhahn@gmail.com>
2018-04-25 18:02:42 +0200

    added Account\\Lernzentrum routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::middleware(['auth'])->group(function() {
     // Lernzentrum
     Route::get('/lernzentrum', 'Home\\LernzentrumController@index')->name('lernzentrum');
     Route::get('/lernzentrum/{lernzentrum}', 'Home\\LernzentrumController@detail')->name('lernzentrum.detail');
+    Route::post('/lernzentrum/{lernzentrum}/signup', 'Home\\LernzentrumController@signup')->name('lernzentrum.signup');
+    Route::post('/lernzentrum/{lernzentrum}/signout', 'Home\\LernzentrumController@signout')->name('lernzentrum.signout');
 
     // Account
     Route::group(['namespace' => 'account', 'prefix' => 'account'], function() {
@@ -29,6 +31,9 @@ Route::middleware(['auth'])->group(function() {
         // Password
         Route::get('/password', 'PasswordController@index')->name('account.password');
         Route::patch('/password', 'PasswordController@update')->name('account.password.update');
+
+        // Lernzentrum
+        Route::get('/lernzentrum', 'LernzentrumController@index')->name('account.lernzentrum');
     });
 });
 
